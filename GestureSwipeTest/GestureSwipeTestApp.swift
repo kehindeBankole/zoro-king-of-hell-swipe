55313cb0f01bbe45088dbdf2939eb24ab80414c9
kehindeBankole <bankolek1@gmail.com>
2024-08-10 18:37:53 +0100

    Initial Commit

---
+++ GestureSwipeTest/GestureSwipeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureSwipeTestApp.swift
+//  GestureSwipeTest
+//
+//  Created by Kehinde Bankole on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GestureSwipeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
